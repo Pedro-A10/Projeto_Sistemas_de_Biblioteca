f7a8cc35630017acc376242e9a766c75195da7f3
Pedro A. <pedroaugusto732@gmail.com>
2025-02-01 13:36:43 -0300

    Update Book.java

--- src/entities/Book.java
+++ src/entities/Book.java
@@ -1,4 +1,41 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Book {
+    private String tittle;
+    private int year;
+
+    private List<Author> autores = new ArrayList<>();
+
+    public Book( String tittle, int year) {
+        this.tittle = tittle;
+        this.year = year;
+    }
+
+    public void addAuthor (Author author) {
+        autores.add(author);
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public List<Author> getAutores() {
+        return autores;
+    }
+
+    public boolean containsAuthor(String authorName) {
+        for (Author author : autores) {
+            if (author.getName().equalsIgnoreCase(authorName)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
