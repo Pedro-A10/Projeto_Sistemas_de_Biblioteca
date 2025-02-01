576b5d985335643b13a88bc8ab54fd49e0ae141b
Pedro A. <pedroaugusto732@gmail.com>
2025-02-01 13:37:21 -0300

    Update Library.java

--- src/entities/Library.java
+++ src/entities/Library.java
@@ -1,4 +1,44 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
+
+    private String name;
+    private List<Book> books = new ArrayList<>();
+
+
+    public Library() {
+    }
+
+    public Library( String name) {
+        this.name = name;
+    }
+
+    public void addBook ( Book book ) {
+        books.add(book);
+    }
+
+    public void removeBook (Book book) {
+        books.remove(book);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public List<Book> searchBookAuthorName(String authorName) {
+        List<Book> resultados = new ArrayList<>();
+        for (Book book : books) {
+            if (book.containsAuthor(authorName)) {
+                resultados.add(book);
+            }
+        }
+        return resultados;
+    }
 }
